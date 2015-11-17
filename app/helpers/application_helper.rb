0e46e828bdb0a4dadd9af13403f84a070915f395
satotoru <satotoru2000@gmail.com>
2015-11-17 21:01:45 +0900

    fix tab indent

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
 
 	# ページごとの完全なタイトルを返します。
-	def full_title(page_titile)
-		base_title = "Ruby on Rails Tutorial Sample App"
-		if page_titile.empty?
-			base_title
-		else
-			"#{base_title} | #{page_titile}"
-		end
-	end
+  def full_title(page_titile)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_titile.empty?
+      base_title
+    else
+      "#{base_title} | #{page_titile}"
+    end
+  end
 end
