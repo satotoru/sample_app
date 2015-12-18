c2b2d22f91457efdbf00d2ab842c21723d04aa92
satotoru <satotoru2000@gmail.com>
2015-12-18 18:31:45 +0900

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,7 +22,15 @@ describe User do
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(admin)
+    end
+  end
+  
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
